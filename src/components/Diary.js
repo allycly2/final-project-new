2b754bd43502848380fab3cddaa219dce0afa18a
allycly2 <148197506+allycly2@users.noreply.github.com>
2024-01-05 16:06:36 +0800

    Delete src directory

--- src/components/Diary.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Diary = () => {
-  // Component logic here
-};
-
-export default Diary;
--- src/components/EventCalendar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EventCalendar = () => {
-  // Component logic here
-};
-
-export default EventCalendar;
